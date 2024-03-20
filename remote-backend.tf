e2ccaa42ae31c1de8bf499729c3c36e3a91ee7f1
jaanu.gudumala@gmail.com <jaanu.gudumala@gmail.com>
2024-03-20 17:07:48 +0530

    Modified backend

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tf-state-terraform-bucket03"
+    bucket = "s3-tf-state-terraform-bucket04"
     region = "us-east-1"
     key = "terraform.tfstate"
     dynamodb_table = "terraform-state-lock-db-table02"
